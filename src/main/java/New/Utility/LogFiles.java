7b02b5ae2da174bf2f786381f1f10da92ded4427
philipp <philipp-wagner-work@outlook.com>
2024-01-21 15:20:39 +0100

    Added comments for LogFiles

--- src/main/java/New/Utility/LogFiles.java
+++ src/main/java/New/Utility/LogFiles.java
@@ -7,6 +7,10 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The LogFiles class is a singleton that manages logging operations for the application.
+ * It provides functionalities to add log entries, and write them to a file on the user's desktop.
+ */
 public class LogFiles {
 
     private static LogFiles instance;
@@ -14,6 +18,11 @@ public class LogFiles {
 
     private LogFiles() {}
 
+    /**
+     * Retrieves the singleton instance of LogFiles. If it doesn't exist, it's created.
+     *
+     * @return The singleton instance of LogFiles.
+     */
     public static synchronized LogFiles getInstance() {
         if (instance == null) {
             instance = new LogFiles();
@@ -21,10 +30,19 @@ public class LogFiles {
         return instance;
     }
 
+    /**
+     * Adds a new log entry to the logs list.
+     *
+     * @param log The log message to be added.
+     */
     public void addToLogs(String log) {
         logs.add(log);
     }
 
+    /**
+     * Writes all the log entries from the logs list to a file named "logs.txt" on the user's desktop.
+     * If the file already exists, it is deleted and created anew before writing the logs.
+     */
     public void write() {
         String desktopPath = System.getProperty("user.home") + "/Desktop";
 
