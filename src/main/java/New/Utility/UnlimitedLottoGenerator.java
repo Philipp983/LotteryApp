24d15e104c5b738c15acbe285af9eee14905b82e
philipp <philipp-wagner-work@outlook.com>
2024-01-21 15:30:41 +0100

    Comments for UnlimitedLottoGenerator

--- src/main/java/New/Utility/UnlimitedLottoGenerator.java
+++ src/main/java/New/Utility/UnlimitedLottoGenerator.java
@@ -7,12 +7,23 @@ import New.Messages.Messages;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * The UnlimitedLottoGenerator class implements Runnable and is designed to continuously generate
+ * lottery numbers for specified lottery games until stopped. It supports different types of lottery games
+ * like 6aus49 and Eurojackpot and allows the exclusion of unlucky numbers.
+ */
 public class UnlimitedLottoGenerator implements Runnable {
 
     private volatile boolean running = true;
     private List<Integer> unluckyNumbers;
     private LotteryGame lotteryGame;
 
+    /**
+     * Constructor to initialize the UnlimitedLottoGenerator with a specific game type and unlucky numbers.
+     *
+     * @param gameType The type of lottery game ("6aus49" or "Eurojackpot").
+     * @param unluckyNumbers A list of numbers to be excluded when generating lottery numbers.
+     */
     public UnlimitedLottoGenerator(String gameType, List<Integer> unluckyNumbers) {
         this.unluckyNumbers = unluckyNumbers;
         switch (gameType) {
@@ -27,6 +38,10 @@ public class UnlimitedLottoGenerator implements Runnable {
         }
     }
 
+    /**
+     * The run method continuously generates lottery numbers based on the specified game type.
+     * It runs in a loop until the 'running' flag is set to false.
+     */
     @Override
     public void run() {
         int index = 1;
@@ -51,6 +66,10 @@ public class UnlimitedLottoGenerator implements Runnable {
         }
     }
 
+    /**
+     * Starts the lottery number generation in a separate thread.
+     * Waits for user input to stop the generation.
+     */
     public void startGenerating() {
         Thread generatorThread = new Thread(this);
         generatorThread.start();
@@ -69,6 +88,10 @@ public class UnlimitedLottoGenerator implements Runnable {
         }
     }
 
+    /**
+     * Stops the lottery number generation by setting the 'running' flag to false.
+     * Also logs the stopping action and writes logs to the file.
+     */
     public void stopGenerating() {
         running = false;
         System.out.println(Messages.CLOSING);
