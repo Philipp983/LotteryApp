a1589ed4f489c768845656fc2306f9199e5aafdf
philipp <philipp-wagner-work@outlook.com>
2024-01-21 15:09:41 +0100

    Added comments for Lotto6aus49

--- src/main/java/New/LotteryModes/Lotto6aus49.java
+++ src/main/java/New/LotteryModes/Lotto6aus49.java
@@ -9,7 +9,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The Lotto6aus49 class extends LotteryGame to implement the specific functionality
+ * for the Lotto 6 aus 49 lottery game. This class is responsible for generating lottery
+ * numbers, considering unlucky numbers, and ensuring that the generated numbers are within
+ * the game's valid range.
+ */
 public class Lotto6aus49 extends LotteryGame {
+    /**
+     * Generates a set of random numbers for the Lotto 6 aus 49 game.
+     * It creates 6 unique random numbers, each between 1 and 49, without any unlucky numbers. The generated numbers are sorted before
+     * being returned.
+     *
+     * @param unluckyNumbers List of numbers to be excluded from the random generation.
+     * @return A list of 6 sorted random numbers for the Lotto 6 aus 49 game.
+     */
     @Override
     public List<Integer> generateRandomNumbers(List<Integer> unluckyNumbers) {
 
@@ -34,6 +48,11 @@ public class Lotto6aus49 extends LotteryGame {
         return randomNumbers;
     }
 
+    /**
+     * Loads and validates the unlucky numbers for the Lotto 6 aus 49 game.
+     * Checks if the unlucky numbers are within the valid range (1 to 49).
+     * If any number is out of range, it throws an IllegalArgumentException.
+     */
     @Override
     public void loadUnluckyNumbers() {
         for (int num : unluckyNumbers) {
@@ -48,6 +67,13 @@ public class Lotto6aus49 extends LotteryGame {
     }
 
 
+    /**
+     * Sets the unlucky numbers for the Lotto 6 aus 49 game.
+     * Overrides the setUnluckyNumbers method from the LotteryGame abstract class to
+     * apply specific validation or processing for the Lotto 6 aus 49 game.
+     *
+     * @param unluckyNumbers List of numbers to set as unlucky for the Lotto 6 aus 49 game.
+     */
     public void setUnluckyNumbers(List<Integer> unluckyNumbers) {
         super.setUnluckyNumbers(unluckyNumbers);
     }
