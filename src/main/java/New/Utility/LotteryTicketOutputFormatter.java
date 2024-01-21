ef7f4d4b4c402bb35cf68b49c12fab0d66ff38f6
philipp <philipp-wagner-work@outlook.com>
2024-01-21 15:22:49 +0100

    Added comments for LotteryTicketOutputFormatter

--- src/main/java/New/Utility/LotteryTicketOutputFormatter.java
+++ src/main/java/New/Utility/LotteryTicketOutputFormatter.java
@@ -3,8 +3,19 @@ package New.Utility;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The LotteryTicketOutputFormatter class is responsible for formatting and displaying
+ * lottery numbers for different lottery games. It provides methods to format numbers
+ * for the 6aus49 and Eurojackpot games.
+ */
 public class LotteryTicketOutputFormatter {
 
+    /**
+     * Formats and prints the output for the 6aus49 lottery game.
+     * Takes a list of random numbers and displays them in a comma-separated format.
+     *
+     * @param randomNumbers List of integers representing the randomly generated numbers for 6aus49.
+     */
     public static void modify6aus49Output(List<Integer> randomNumbers) {
         // Formatting the output
         System.out.println("\n6aus49 Tippzahlen: " + randomNumbers.stream()
@@ -12,6 +23,12 @@ public class LotteryTicketOutputFormatter {
                 .collect(Collectors.joining(", ")));
     }
 
+    /**
+     * Formats and prints the output for the Eurojackpot lottery game.
+     * Separates the list into two parts: 5aus50 and 2aus12, and displays them in a comma-separated format.
+     *
+     * @param randomNumbers List of integers representing the randomly generated numbers for Eurojackpot.
+     */
     public static void modifyEurojackpotOutput(List<Integer> randomNumbers) {
         List<Integer> numbers5aus50 = randomNumbers.subList(0, 5);
         List<Integer> numbers2aus12 = randomNumbers.subList(5, 7);
